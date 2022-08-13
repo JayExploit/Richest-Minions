0af4d9d57f7d65c1362bdcb946da32be6ff62088
jaaat <80226697+JayExploit@users.noreply.github.com>
2022-08-13 14:21:21 -0400

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -11,7 +11,7 @@ local Config = {
     NamesColor        = Color3.fromRGB(255,255,255),
     NamesOutlineColor = Color3.fromRGB(0,0,0),
     NamesFont         = 2, -- 0,1,2,3
-    NamesSize         = 18
+    NamesSize         = 13
 }
 
 function CreateEsp(Player)
@@ -67,11 +67,11 @@ function CreateEsp(Player)
                 HealthBar.Filled = true
                 HealthBar.ZIndex = 69
                 if Config.HealthBarSide == "Left" then
-                    HealthBarOutline.Size = Vector2.new(20,height)
+                    HealthBarOutline.Size = Vector2.new(2,height)
                     HealthBarOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2) + Vector2.new(-3,0)
                     
-                    HealthBar.Size = Vector2.new(15,-(HealthBarOutline.Size.Y - 2) * (Player.Character:FindFirstChild("Humanoid").Health/Player.Character:FindFirstChild("Humanoid").MaxHealth))
-                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(0.5,-15 + HealthBarOutline.Size.Y)
+                    HealthBar.Size = Vector2.new(10,-(HealthBarOutline.Size.Y - 20) * (Player.Character:FindFirstChild("Humanoid").Health/Player.Character:FindFirstChild("Humanoid").MaxHealth))
+                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(1,-1 + HealthBarOutline.Size.Y)
                 elseif Config.HealthBarSide == "Bottom" then
                     HealthBarOutline.Size = Vector2.new(width,3)
                     HealthBarOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2) + Vector2.new(0,height + 2)
